d8d1748ff10f58213aff4976d1d7bbc269e3fae4
Legenda <kostrov2525@gmail.com>
2023-02-08 18:00:02 +0500

    Готовимся к миссии

--- main.py
+++ main.py
@@ -9,5 +9,11 @@ def second(text):
     return render_template('index.html', list=list_prof, text=text)
 
 
+@app.route('/<title>')
+@app.route('/index/<title>')
+def index(title):
+    return render_template('base.html', title=title)
+
+
 if __name__ == '__main__':
     app.run(port=8080, debug=True)
