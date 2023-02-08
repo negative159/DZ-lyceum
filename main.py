11b4f18015b8e22d781a6da72ad8846b1df26f31
Legenda <kostrov2525@gmail.com>
2023-02-08 18:32:58 +0500

    Автоматический ответ

--- main.py
+++ main.py
@@ -15,5 +15,15 @@ def index(title):
     return render_template('base.html', title=title)
 
 
+@app.route('/answer')
+@app.route('/auto_answer')
+def answer():
+    form = {'surname': 'Стив Джобс', 'name': 'Марк Цукерберг',
+            'education': 'GLOBAL (10 LVL FaceIT)', 'profession': 'Охотник на монстров',
+            'sex': 'Альфа', 'motivation': 'Поесть во Вкусно и Точка',
+            'ready': 'Вы готовы дети? True'}
+    return render_template('auto_answer.html', **form)
+
+
 if __name__ == '__main__':
     app.run(port=8080, debug=True)
